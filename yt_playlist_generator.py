7e0cbfc93a5f0ff256921199f33cd3669f2f82db
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-10-30 00:14:10 +0700

    split into playlists consisting of 50

--- yt_playlist_generator.py
+++ yt_playlist_generator.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 # yt_playlist_generator.py - Creates a YouTube playlist from a list of URLs.
 
+limit = 50
+url_file_name = "urls.txt"
+playlist_file_name = "playlists.txt"
+
 def main():
-    file = open('urls.txt')
+    file = open(url_file_name)
     urls = [line.strip() for line in file.readlines()]
     file.close()
     ids = [url[-11:] for url in urls]
-    playlist = 'https://www.youtube.com/watch_videos?video_ids=' + ','.join(ids)
-    print(f'Playlist created at: {playlist}')
+    print('Playlist created at:')
+    playlists = []
+    for i in range(len(ids)//limit):
+        start = i * limit
+        end = (i+1) * limit
+        playlist = 'https://www.youtube.com/watch_videos?video_ids=' + ','.join(ids[start:end])
+        print(playlist)
+        playlists.append(playlist)
+    with open(playlist_file_name, 'w') as f:
+        f.writelines([f"{line}\n" for line in playlists])
 
 if __name__ == '__main__':
     main()
